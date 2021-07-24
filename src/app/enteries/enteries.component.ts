ab2a6a0b7b3d561a390e2fddeee7983a7586d295
sourabhgupta3838@gmail.com <sourabhg@ustechsolutions.com>
2021-07-24 22:50:55 +0530

    updated datatable

--- src/app/enteries/enteries.component.ts
+++ src/app/enteries/enteries.component.ts
@@ -1,9 +1,12 @@
 import { HttpParams } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { Enteries } from '../models/enteries.model';
 import { EnteriesService } from '../_services/enteries.service';
 import { TokenStorageService } from '../_services/token-storage.service';
+
+import { DataTableDirective } from 'angular-datatables';
+
 
 @Component({
   selector: 'app-enteries',
@@ -11,32 +14,52 @@ import { TokenStorageService } from '../_services/token-storage.service';
   styleUrls: ['./enteries.component.css']
 })
 export class EnteriesComponent implements OnInit {
-
+  @ViewChild(DataTableDirective, { static: false })
+  private datatableElement: DataTableDirective;
   dtOptions: DataTables.Settings = {};
-  public page = 1;
-  public pageSize = 10;
-  itemsPerPage = 10;
+  itemsPerPage: number = 20;
   totalItems: any;
+  page: any = 1;
+  previousPage: any;
+  gotData: boolean = false;
 
   constructor(private enteriesService: EnteriesService, 
               private tokenStorageService: TokenStorageService) { }
 
   enteries?: Enteries[];
+
   ngOnInit(): void {
-    console.log(this.tokenStorageService.getToken()); 
-    this.getEnteries();
+    //this.getEnteries();
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 2,
+      serverSide: true,
+      processing: true,
+      lengthChange: false,
+      ajax: (dataTablesParameters: any, callback) => {
+        const params = new HttpParams()
+          .set('offset', dataTablesParameters.draw - 1)
+          .set('limit', dataTablesParameters.length);
+        this.enteriesService.readAll(params)
+          .subscribe(
+            (res: any) => {
+              this.enteries = res.data;
+              callback({
+                recordsTotal: res.recordsTotal,
+                recordsFiltered: res.recordsFiltered,
+                data: []
+              });
+            });
+      }
+    };
   }
 
-  getPageSymbol(current: number) {
-    return ['A', 'B', 'C', 'D', 'E', 'F', 'G'][current - 1];
+  loadPage(page: number) {
+    if (page !== this.previousPage) {
+      this.previousPage = page;
+      this.getEnteries();
+    }
   }
-
-  selectPage(page: string) {
-    this.page = parseInt(page, 10) || 1;
-    console.log(this.page);
-    this.getEnteries();
-  }
-
 
   getEnteries(): void {
 
@@ -47,13 +70,29 @@ export class EnteriesComponent implements OnInit {
     this.enteriesService.readAll(params)
     .subscribe(
       (res: any) => {
-        console.log("response", res);
+        this.gotData = true;
         this.totalItems = res.resultCount;
-        console.log("tl---", this.totalItems);
         this.enteries = res.data;
+        this.dtOptions = {
+          pagingType: 'full_numbers',
+          pageLength: 2,
+          serverSide: true,
+          processing: true,
+          lengthChange: false,
+          data: res.data,
+          columns: [
+            { title: 'TYPE', data: 'type' },
+            { title: 'PART ID', data: 'partId' },
+            { title: 'Name', data: 'name' },
+            { title: 'STATUS', data: 'status' },
+            { title: 'CREATED', data: 'creationTime' },
+          ]
+        };
       },
       err => {
-        console.log("enteries error", err);        
+        console.log("enteries error", err);
+        //this.dataTable = $(this.table.nativeElement);
+        //this.dataTable.DataTable(this.dtOptions);
       }
     );
   }
